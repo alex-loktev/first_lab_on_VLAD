58d81d22fa332bebb652d1577e7f6c5f9f42e71e
shmarovoz <loktev.lesha@gmail.com>
2019-09-23 20:58:12 +0300

    initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectCount = new System.Windows.Forms.Button();
+            this.operationsPanel = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.operationsPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(247, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите количество переменных";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите столбец значений функции:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(254, 76);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(92, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Номер строки";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(408, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Значение функции";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.comboBox3.Location = new System.Drawing.Point(411, 76);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Сохранить значение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // selectCount
+            // 
+            this.selectCount.Location = new System.Drawing.Point(344, 11);
+            this.selectCount.Name = "selectCount";
+            this.selectCount.Size = new System.Drawing.Size(75, 23);
+            this.selectCount.TabIndex = 8;
+            this.selectCount.Text = "Начать";
+            this.selectCount.UseVisualStyleBackColor = true;
+            this.selectCount.Click += new System.EventHandler(this.SelectCount_Click);
+            // 
+            // operationsPanel
+            // 
+            this.operationsPanel.Controls.Add(this.button3);
+            this.operationsPanel.Controls.Add(this.button2);
+            this.operationsPanel.Controls.Add(this.comboBox3);
+            this.operationsPanel.Controls.Add(this.label2);
+            this.operationsPanel.Controls.Add(this.button1);
+            this.operationsPanel.Controls.Add(this.comboBox2);
+            this.operationsPanel.Controls.Add(this.label3);
+            this.operationsPanel.Controls.Add(this.label4);
+            this.operationsPanel.Enabled = false;
+            this.operationsPanel.Location = new System.Drawing.Point(29, 63);
+            this.operationsPanel.Name = "operationsPanel";
+            this.operationsPanel.Size = new System.Drawing.Size(559, 291);
+            this.operationsPanel.TabIndex = 9;
+            this.operationsPanel.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.selectCount);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(28, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(559, 45);
+            this.panel1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 21);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(443, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 21);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Вычислить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.operationsPanel);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.operationsPanel.ResumeLayout(false);
+            this.operationsPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button selectCount;
+        private System.Windows.Forms.GroupBox operationsPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string cknf = "";
+        string cdnf = "";
+
+        int[] mass;
+
+        int[,] mass1 =  {
+                                {0,0,1,1},
+                                {0,1,0,1}
+                            };
+
+        int[,] mass2 =  {
+                                {0,0,0,0,1,1,1,1},
+                                {0,0,1,1,0,0,1,1},
+                                {0,1,0,1,0,1,0,1}
+                            };
+
+        int[,] mass3 =  {
+                                {0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
+                                {0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1},
+                                {0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1},
+                                {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1}
+                            };
+        int selectedIndex;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            ComboboxItems(comboBox1.SelectedIndex);
+        }
+
+        public void ComboboxItems( int selectedIndex)
+        {
+            comboBox2.Items.Clear();
+            for (int i = 1; i <= (int)(Math.Pow(2,selectedIndex+2)); i++)
+            {
+                comboBox2.Items.Add(i);
+            }
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            mass[comboBox2.SelectedIndex] = Convert.ToInt32(comboBox3.SelectedItem);
+        }
+
+        private void SelectCount_Click(object sender, EventArgs e)
+        {
+            mass = new int[(int)(Math.Pow(2, comboBox1.SelectedIndex + 2))];
+            operationsPanel.Enabled = true;
+            panel1.Enabled = false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            operationsPanel.Enabled = false;
+            panel1.Enabled = true;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            
+            if (comboBox1.SelectedIndex == 2)
+            {
+                for (int i = 0; i < mass1.GetLength(1); i++)
+                {
+                    if (mass[i] == 1)
+                    {
+                        switch (mass1[0, i])
+                        {
+                            case 1:
+                                cknf = cknf + " X&";
+                                break;
+                            case 0:
+                                cknf = cknf + " ¬X&";
+                                break;
+                        }
+                        switch (mass1[1, i])
+                        {
+                            case 1:
+                                cknf = cknf + "Y V";
+                                break;
+                            case 0:
+                                cknf = cknf + "¬Y V";
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        switch (mass1[0, i])
+                        {
+                            case 1:
+                                cdnf = cdnf + " (XV";
+                                break;
+                            case 0:
+                                cdnf = cdnf + " (¬XV";
+                                break;
+                        }
+                        switch (mass1[1, i])
+                        {
+                            case 1:
+                                cdnf = cdnf + "Y) &";
+                                break;
+                            case 0:
+                                cdnf = cdnf + "¬Y) &";
+                                break;
+                        }
+                    }
+                }
+                MessageBox.Show(cknf);
+
+            }
+        }
+    }
+}
